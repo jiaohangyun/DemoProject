2c554287a5b8154357ae51d43a4eeaab415ff8d0
jiaohangyun <617493711@qq.com>
2019-12-17 15:32:03 +0800

    tijiao

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Date date = new Date();
-        
+        Scanner s = new Scanner(System.in);
     }
 }
